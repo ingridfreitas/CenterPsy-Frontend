a9e0c0f8706f8062e251df6db60ab46750dc2268
Ingrid Heloise <ingridheloisesantos@gmail.com>
2026-09-06 15:17:01 -0300

    Capa - Ingrid
    A tela inicial com a capa e responsividade foi feita

--- src/app/capa/capa.ts
+++ src/app/capa/capa.ts
@@ -1,11 +1,20 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-capa',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './capa.html',
   styleUrl: './capa.css',
 })
 export class Capa {
+  isMenuOpen = false;
 
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+
+  closeMenu() {
+    this.isMenuOpen = false;
+  }
 }
